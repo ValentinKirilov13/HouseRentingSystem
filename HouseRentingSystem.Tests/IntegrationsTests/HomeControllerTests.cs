44f2921ac45b4ae4d957b64d5f6cedcc704293c7
ValentinKirilov13 <valkilmaar@gmail.com>
2026-06-06 16:43:22 +0300

    Add project files.

---
+++ HouseRentingSystem.Tests/IntegrationsTests/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using HouseRentingSystem.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Tests.IntegrationsTests
+{
+    public class HomeControllerTests
+    {
+        private HomeController homeController;
+
+        [OneTimeSetUp]
+        public void SetUp()
+            => this.homeController = new HomeController(null);
+
+        [OneTimeTearDown]
+        public void TearDown() => homeController.Dispose();
+
+        [Test]
+        public void Error_ShouldReturnCorrectView()
+        {
+            // Arrange: assign a valid status code to a variable
+            var statusCode = 500;
+
+            // Act: invoke the controller method with valid data
+            var result = this.homeController.Error(statusCode);
+
+            // Assert the returned result is not null
+            Assert.IsNotNull(result);
+
+            // Assert the returned result is a view
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+        }
+    }
+}
---
+++ HouseRentingSystem.Tests/Mocks/MapperMock.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HouseRentingSystem.Services.Infrastructure;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace HouseRentingSystem.Tests.Mocks
+{
+    public static class MapperMock
+    {
+        public static IMapper Instance
+        {
+            get
+            {
+                var mapperConfiguration = new MapperConfiguration(config =>
+                {
+                    config.AddProfile<ServiceMappingProfile>();
+                }, NullLoggerFactory.Instance);
+
+                return new Mapper(mapperConfiguration);
+            }
+        }
+    }
+}
